e9dc74db74971352ea7a575d4e44e6b384629f27
Peter Love <p.love@lancaster.ac.uk>
2012-11-28 16:27:28 +0000

    cleanup

--- wsgi.py
+++ wsgi.py
@@ -12,24 +12,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atl.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-#import sys
-#
-#sys.path.insert(0, '/var/local/django/atl')
-#
-#import settings
-#
-#import django.core.management
-#django.core.management.setup_environ(settings)
-#utility = django.core.management.ManagementUtility()
-#command = utility.fetch_command('runserver')
-#
-#command.validate()
-#
-#import django.conf
-#import django.utils
-#
-#django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
-#
-#import django.core.handlers.wsgi
-#
-#application = django.core.handlers.wsgi.WSGIHandler()
