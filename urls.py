3f2dc5c5f00493db8bb462f00f20018772f792d2
Peter Love <p.love@lancaster.ac.uk>
2013-01-15 12:39:14 +0000

    possible api app

--- urls.py
+++ urls.py
@@ -5,8 +5,12 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
+    # rendered web pages
     (r'^$', include('atl.mon.urls')),
-    (r'^admin/', include(admin.site.urls)),
     (r'^mon/', include('atl.mon.urls')),
+    (r'^admin/', include(admin.site.urls)),
     (r'^kit/', include('atl.kit.urls')),
+    # maybe apfmon API as separate app
+#    (r'^api/', include('atl.api.urls')),
+#    python manage.py startapp api
 )
