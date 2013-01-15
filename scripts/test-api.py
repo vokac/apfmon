7d0d0a16b9f8e8091b3f31d4cc11834eaee48a9b
Peter Love <p.love@lancaster.ac.uk>
2013-01-15 15:54:34 +0000

    good api test

--- scripts/test-api.py
+++ scripts/test-api.py
@@ -5,20 +5,35 @@ import requests
 crlist = []
 
 baseuri = 'http://apfmon.lancs.ac.uk/api/'
+baseuri = 'http://py-dev.lancs.ac.uk:8123/api/'
 urlcr = 'http://apfmon.lancs.ac.uk/mon/c'
 
-cid=str(random.randint(1,10000))
-nick='ANALY_BHAM'
-fid='peter-uk-dev'
-label='ANALY_BHAM'
-d=(cid,nick,fid,label)
+nick = 'ANALY_LANCS'
+factory = 'peter-uk-dev'
+label = 'ANALY_LANCS-api'
 
-crlist.append(d)
-j=json.JSONEncoder()
-jmsg = j.encode(crlist)
+jid = str(random.randint(1,10000))
+j1 = {
+      'jid'     : jid,
+      'nick'    : nick,
+      'factory' : factory,
+      'label'   : label,
+    }
 
-r = requests.post(baseuri + 'c', data=jmsg)
+jid = str(random.randint(1,10000))
+j2 = {
+      'jid'     : jid,
+      'nick'    : nick,
+      'factory' : factory,
+      'label'   : label,
+    }
+
+# payload is a list of dicts defining individual jobs
+payload = [j1, j2]
+url = baseuri + 'jobs2'
+r = requests.post(url, data=json.dumps(payload))
 print 'OK?', r.ok
+print 'URL', r.url
 print 'STATUS_CODE', r.status_code
 print 'CONTENT-TYPE', r.headers['content-type']
-#print 'TEXT:', r.text
+print 'TEXT:', r.text
