09cf1d96ff1c77850cf8546e7918a090a8767e8e
Peter Love <p.love@lancaster.ac.uk>
2013-01-18 10:31:26 +0000

    api stuff

--- scripts/test-api.py
+++ scripts/test-api.py
@@ -5,24 +5,23 @@ import requests
 crlist = []
 
 baseuri = 'http://apfmon.lancs.ac.uk/api/'
-baseuri = 'http://py-dev.lancs.ac.uk:8123/api/'
-urlcr = 'http://apfmon.lancs.ac.uk/mon/c'
+baseuri = 'http://py-dev.lancs.ac.uk:8000/api/'
 
-nick = 'ANALY_LANCS'
+nick = 'ANALY_LANCS-nick'
 factory = 'peter-uk-dev'
-label = 'ANALY_LANCS-api'
+label = 'ANALY_LANCS-label'
 
-jid = str(random.randint(1,10000))
+cid = str(random.randint(1,10000))
 j1 = {
-      'jid'     : jid,
+      'cid'     : cid,
       'nick'    : nick,
       'factory' : factory,
       'label'   : label,
     }
 
-jid = str(random.randint(1,10000))
+cid = str(random.randint(1,10000))
 j2 = {
-      'jid'     : jid,
+      'cid'     : cid,
       'nick'    : nick,
       'factory' : factory,
       'label'   : label,
@@ -31,9 +30,62 @@ j2 = {
 # payload is a list of dicts defining individual jobs
 payload = [j1, j2]
 url = baseuri + 'jobs2'
-r = requests.post(url, data=json.dumps(payload))
+r = requests.put(url, data=json.dumps(payload))
 print 'OK?', r.ok
 print 'URL', r.url
 print 'STATUS_CODE', r.status_code
 print 'CONTENT-TYPE', r.headers['content-type']
 print 'TEXT:', r.text
+
+# /factories GET
+url = baseuri + 'factories'
+r = requests.get(url)
+print 'OK?', r.ok
+print 'URL', r.url
+print 'STATUS_CODE', r.status_code
+print 'CONTENT-TYPE', r.headers['content-type']
+print 'TEXT:', r.text
+
+print "TEST: GET /factories/some-factory of an existing factory"
+url = baseuri + '/'.join(('factories',factory))
+r = requests.get(url)
+print 'OK?', r.ok
+print 'URL', r.url
+print 'STATUS_CODE', r.status_code
+print 'CONTENT-TYPE', r.headers['content-type']
+print 'TEXT:', r.text
+
+print "TEST: GET /factories/some-factory of a non-existing factory"
+url = baseuri + '/'.join(('factories','notme'))
+r = requests.get(url)
+print 'OK?', r.ok
+print 'URL', r.url
+print 'STATUS_CODE', r.status_code
+print 'CONTENT-TYPE', r.headers['content-type']
+
+print "TEST: PUT /factories/some-new-factory non-existing factory"
+factory = '-'.join(('new',str(random.randint(100,999))))
+url = baseuri + '/'.join(('factories',factory))
+f = {
+     'url'     : 'http://localhost/',
+     'email'   : 'p.love@lancaster.ac.uk',
+     'version' : '0.0.1',
+    }
+r = requests.put(url, data=json.dumps(f))
+print 'OK?', r.ok
+print 'URL', r.url
+print 'STATUS_CODE', r.status_code
+print 'CONTENT-TYPE', r.headers['content-type']
+print 'TEXT:', r.text
+
+print "TEST: GET /jobs/some-jobid of existing job"
+jid = ':'.join(('peter-uk-dev',j2['cid']))
+jid = ':'.join(('peter-uk-dev','1237017.0'))
+url = baseuri + '/'.join(('jobs2',jid))
+r = requests.get(url)
+print 'OK?', r.ok
+print 'URL', r.url
+print 'STATUS_CODE', r.status_code
+print 'CONTENT-TYPE', r.headers['content-type']
+print 'TEXT:', r.text
+
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +12,8 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^kit/', include('atl.kit.urls')),
     # maybe apfmon API as separate app
-#    (r'^api/', include('atl.api.urls')),
+    (r'^api/', include('atl.mon.urls')),
 #    python manage.py startapp api
 )
+
+urlpatterns += staticfiles_urlpatterns()
