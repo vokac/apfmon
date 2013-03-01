eef340a1b71b2259e79aa48b9cc161cb387267d9
Peter Love <p.love@lancaster.ac.uk>
2013-03-01 10:31:04 +0000

    optparser to allow time threshold argument

--- scripts/mon-cleandb.py
+++ scripts/mon-cleandb.py
@@ -1,4 +1,6 @@
+from datetime import timedelta, datetime
 import logging
+from optparse import OptionParser
 import pytz
 import statsd
 import sys
@@ -7,19 +9,49 @@ from time import time
 sys.path.append('/var/local/django')
 
 from atl.mon.models import Job
-from datetime import timedelta, datetime
 from django.core.cache import cache
 
 """
-Remove jobs from DB which have been in DONE/FAULT state for 24hrs
+Remove jobs from DB which have been in DONE/FAULT state for
+a set number of hours
 """
 
-def main():
-    # clean pyf Job
-    dt = datetime.now(pytz.utc) - timedelta(hours=32)
+def main(c):
+    parser = OptionParser(usage='''%prog [OPTIONS]
+Delete job records which have been in a final state longer
+than the specified number of hours
+''')
+    parser.add_option("-t",
+                       dest="t",
+                       default=24,
+                       action="store",
+                       type="int",
+                       help="Set number of hours for cleaning threshold [default 24]")
+    parser.add_option("-q", "--quiet",
+                       dest="logLevel",
+                       default=logging.WARNING,
+                       action="store_const",
+                       const=logging.WARNING,
+                       help="Set logging level to WARNING [default]")
+    parser.add_option("-v", "--info",
+                       dest="logLevel",
+                       default=logging.WARNING,
+                       action="store_const",
+                       const=logging.INFO,
+                       help="Set logging level to INFO [default WARNING]")
+
+    (options, args) = parser.parse_args()
+
+    dt = datetime.now(pytz.utc) - timedelta(hours=options.t)
     djobs = Job.objects.filter(state__name='DONE', last_modified__lt=dt)
     fjobs = Job.objects.filter(state__name='FAULT', last_modified__lt=dt)
     
+    msg = 'DONE: %d' % djobs.count()
+    logging.info(msg)
+    msg = 'FAULT: %d' % fjobs.count()
+    logging.info(msg)
+    c.gauge('apfmon.dclean',djobs.count())
+    c.gauge('apfmon.fclean',fjobs.count())
     djobs.delete()
     fjobs.delete()
 
@@ -74,7 +106,7 @@ if __name__ == "__main__":
     c = statsd.StatsClient(host='py-heimdallr', port=8125)
     stat = 'apfmon.monclean'
     start = time()
-    rc = main()
+    rc = main(c)
     elapsed = time() - start
     c.timing(stat,int(elapsed))
     msg = "mon-clean.py elapsed time: %d" % int(elapsed)
