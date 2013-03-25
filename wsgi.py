b434c8bcf2110faf945cf7e347ffcdfd95a1559f
Peter Love <p.love@lancaster.ac.uk>
2013-03-25 12:29:57 +0000

    revert to generic django wsgi.py, virtualenv stuff handled with modified pythonpath

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,29 @@
-venv_django_root = '/var/local/django'
-try:
-    activate_this = venv_django_root + '/apfmon/env/bin/activate_this.py'
-    execfile(activate_this, dict(__file__=activate_this))
+"""
+WSGI config for apfmon project.
 
-    import os
-    import sys
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-    sys.path.insert(0, venv_django_root)
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
 
-except IOError: # not running under venv
-    import os
-    import sys
-
-    newpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.insert(0, newpath)
-    sys.path.insert(0, newpath+"/apfmon")
+"""
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apfmon.settings")
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
 
