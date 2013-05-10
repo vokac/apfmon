e82da789643bf2f40d9bb10e563a8651992801e3
Peter Love <p.love@lancaster.ac.uk>
2013-05-10 17:18:12 +0100

    tidy

--- settings/settings.py
+++ settings/settings.py
@@ -1,8 +1,5 @@
 # Django settings for atl project.
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -25,15 +22,6 @@ USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-#MEDIA_ROOT = '/var/www/html/atl/media'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-#MEDIA_URL = 'http://apfmon.lancs.ac.uk/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
