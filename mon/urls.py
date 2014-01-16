4724766abc7570318b80f440c5c3b4727e1b5e06
Peter Love <p.love@lancaster.ac.uk>
2014-01-16 15:04:18 +0000

    remove old urls

--- mon/urls.py
+++ mon/urls.py
@@ -4,18 +4,11 @@ urlpatterns = patterns('apfmon.mon.views',
     (r'^$', 'index'),
 # rendered views
     (r'^cloud/(?P<name>[a-zA-Z]+)/?$', 'cloud'),
-    (r'^factory/(?P<fid>\S*)/?$', 'factory'),
-    (r'^queues/?$', 'queues'),
-    (r'^l/(?P<lid>\d*)/page/(?P<p>\d*)$', 'label'),
-    (r'^l/(?P<lid>\d*)/+$', 'label'),
     (r'^report/?$', 'report'),
     (r'^site/(?P<sid>\S*)$', 'site'),
     (r'^help/?$', 'help'),
     (r'^q/(?P<qid>\d*)/page/(?P<p>\d*)$', 'pandaq'),
     (r'^q/(?P<qid>\S*)/?$', 'pandaq'),
-    (r'^job1/(?P<fid>\S*)/(?P<cid>\S*)/?$', 'job1'),
-    (r'^jobs1/(?P<lid>\d*)/(?P<state>[A-Z]*)/page/(?P<p>\d*)$', 'jobs1'),
-    (r'^jobs1/(?P<lid>\d*)/(?P<state>[A-Z]*)/?$', 'jobs1'),
     (r'^debug/?$', 'debug'),
     (r'^test/?$', 'test'),
     (r'^stats/?$', 'stats'),
@@ -34,8 +27,3 @@ urlpatterns = patterns('apfmon.mon.views',
     (r'(?P<fname>[\w-]*)/(?P<item>\S*[^/])$', 'singleitem'),
     (r'(?P<fname>[\w-]*)$', 'singlefactory'),
 )
-
-"""
-django.views.generic.date_based.archive_index
-http://docs.djangoproject.com/en/dev/ref/generic-views/
-"""
