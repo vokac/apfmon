de6a2a142734357ee72d6253869c3a49c7320233
Peter Love <palove@gmail.com>
2016-10-25 23:50:34 +0100

    update to django 1.10

---
+++ apfmon/urls.py
@@ -0,0 +1,13 @@
+# https://docs.djangoproject.com/en/1.10/topics/http/urls/
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin', admin.site.urls),
+#    url(r'^kit/', include('kit.urls')),
+    url(r'^api/', include('api.urls')),
+#    url(r'^mon/', include('mon.urls')),
+    url(r'^', include('mon.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
