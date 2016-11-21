81591041ecb1bd794ed38f02edf2a9e9af007ec9
Peter Love <palove@gmail.com>
2016-11-21 14:00:06 +0000

    alloed hosts

--- apfmon/settings.py
+++ apfmon/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', 'py-prod.lancs.ac.uk']
+ALLOWED_HOSTS = ['localhost', 'py-prod.lancs.ac.uk', 'apfmon.lancs.ac.uk']
 
 # Application definition
 
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -106,6 +106,28 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'apfmon': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    },
+}
+
+
 
 # Import local settings
 try:
