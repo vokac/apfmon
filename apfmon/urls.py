71e2a49e3637256e665968ae25f86467e7960183
Peter Love <palove@gmail.com>
2017-05-05 11:23:27 +0100

    add debugtoolbar

--- apfmon/urls.py
+++ apfmon/urls.py
@@ -1,5 +1,6 @@
 # https://docs.djangoproject.com/en/1.10/topics/http/urls/
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -12,3 +13,9 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
